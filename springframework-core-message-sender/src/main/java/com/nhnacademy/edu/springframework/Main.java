cb20bbd29487aa7bef28df7e42718bc1f0d5e06f
sangwoong12 <timeattacks2@naver.com>
2023-04-11 23:56:24 +0900

    first commit

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -2,6 +2,16 @@ package com.nhnacademy.edu.springframework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main main = new Main();
+        User user = new User("timeattacks2@naver.com", "010-4596-5429");
+        main.sendEmailMessage(user,"hello");
+        main.sendSmsMessage(user,"hello");
+
+    }
+    public void sendSmsMessage(User user,String message){
+        System.out.println("Sms Message Sent to"+user.getPhoneNumber() + " : " +message);
+    }
+    public void sendEmailMessage(User user,String message){
+        System.out.println("Email Message Sent to"+user.getEmail() + " : " +message);
     }
 }
