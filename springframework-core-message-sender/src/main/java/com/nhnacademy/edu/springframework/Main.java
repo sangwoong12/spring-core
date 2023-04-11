34a8ad77e010a98777270e0213207d70729eecca
sangwoong12 <timeattacks2@naver.com>
2023-04-12 01:58:36 +0900

    AnnotationConfigApplicationContext

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,11 +1,13 @@
 package com.nhnacademy.edu.springframework;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
-            context.getBean("messageSendService", MessageSendService.class).doSendMessage();
+        try (AnnotationConfigApplicationContext context =
+                     new AnnotationConfigApplicationContext("com.nhnacademy.edu.springframework.config")){
+            MessageSendService messageSendService = context.getBean("messageSendService", MessageSendService.class);
+            messageSendService.doSendMessage();
         }
     }
 }
--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/MessageSendService.java
@@ -2,18 +2,11 @@ package com.nhnacademy.edu.springframework;
 
 import com.nhnacademy.edu.springframework.message_sender.MessageSender;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public class MessageSendService {
-//    @Autowired
+    @Autowired
     private MessageSender messageSender;
 
-    //Type 이 하나만 존재할때 Autowired를 할경우 beans.xml 에 명시해주지 않아도 자동으로 주입된다. 생성자 없이 field 에 선언해주어도 된다.
-    //2개 이상일 경우 Qualifier 로 지정해줄수 있다.
-    @Autowired
-    public void setMessageSender(@Qualifier("smsMessageSender") MessageSender messageSender) {
-        this.messageSender = messageSender;
-    }
 
     public void doSendMessage() {
         User user = new User("timeattacks2@naver.com", "010-4596-5429");
