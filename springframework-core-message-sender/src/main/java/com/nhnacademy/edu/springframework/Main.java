36ee1e15e1427a9ef6ebd46d6e64e0be03066937
sangwoong12 <timeattacks2@naver.com>
2023-04-12 00:36:20 +0900

    DI constructor

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,15 +1,12 @@
 package com.nhnacademy.edu.springframework;
 
-import com.nhnacademy.edu.springframework.message_sender.MessageSender;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
-            // destroy() -> destory 순으로 init 의 역순으로 진행.
-            System.out.println("---------- doSendMessage ----------");
-            new MessageSendService(context.getBean("smsMessageSender", MessageSender.class)).doSendMessage();
-            System.out.println("---------- doSendMessage ----------");
+            //constructor ref 로 명시하기 때문에 같은 Type 이 존재해서 에러발생 X
+            context.getBean("messageSendService",MessageSendService.class).doSendMessage();
         }
     }
 }
--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message_sender/SmsMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message_sender/SmsMessageSender.java
@@ -1,30 +1,10 @@
 package com.nhnacademy.edu.springframework.message_sender;
 
 import com.nhnacademy.edu.springframework.User;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class SmsMessageSender implements MessageSender, InitializingBean, DisposableBean {
-    public SmsMessageSender() {
-        System.out.println("SmsMessageSender : initiated");
-    }
-
-    @Override
-    public void afterPropertiesSet() {
-        System.out.println("    > SmsMessageSender : afterPropertiesSet called");
-    }
-    public void init(){
-        System.out.println("        ㄴ SmsMessageSender : init called");
-    }
+public class SmsMessageSender implements MessageSender {
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("Sms Message Sent to " + user.getPhoneNumber() + " : " + message);
-    }
-    public void destory(){
-        System.out.println("    ㄴ SmsMessageSender : destory called");
-    }
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("        > SmsMessageSender : destroy called");
     }
 }
