1c0893429a1e52bada9cea00abfa9e472ef139dc
sangwoong12 <timeattacks2@naver.com>
2023-04-12 02:17:50 +0900

    Spring JavaConfig DI

--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/MessageSendService.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/MessageSendService.java
@@ -1,13 +1,13 @@
 package com.nhnacademy.edu.springframework;
 
 import com.nhnacademy.edu.springframework.message_sender.MessageSender;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class MessageSendService {
-    @Autowired
     private MessageSender messageSender;
 
-
+    public MessageSendService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
     public void doSendMessage() {
         User user = new User("timeattacks2@naver.com", "010-4596-5429");
         messageSender.sendMessage(user, "hello");
--- springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message_sender/SmsMessageSender.java
+++ springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/message_sender/SmsMessageSender.java
@@ -7,10 +7,4 @@ public class SmsMessageSender implements MessageSender {
     public void sendMessage(User user, String message) {
         System.out.println("Sms Message Sent to " + user.getPhoneNumber() + " : " + message);
     }
-    public void init(){
-        System.out.println("SmsMessageSender init called");
-    }
-    public void close(){
-        System.out.println("SmsMessageSender close called");
-    }
 }
